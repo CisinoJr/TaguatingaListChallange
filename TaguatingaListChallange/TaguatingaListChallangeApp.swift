8d8d2ba59b903c8ee44523a68c22093753acbb5e
Cisino Junior <cisino.gomes@stone.com.br>
2023-05-01 17:00:05 -0300

    Initial Commit

---
+++ TaguatingaListChallange/TaguatingaListChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaguatingaListChallangeApp.swift
+//  TaguatingaListChallange
+//
+//  Created by Cisino Junior on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TaguatingaListChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
